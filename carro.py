6ac829f20a82338784e5d7d62657a322c36d1749
Eduardo <capaci-thor@MacBook-Pro-de-Eduardo.local>
2022-07-09 17:12:00 -0500

    modificación para grabar video y guardarlo

--- carro.py
+++ carro.py
@@ -5,13 +5,20 @@ import time
 capture = cv2.VideoCapture(0)
 car = YB_Pcb_Car.YB_Pcb_Car()
 
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'avc1')
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
+
 while (capture.isOpened()):
     ret, frame = capture.read()
-    cv2.imshow('webCam',frame)
-    car.Car_Run(150,150)
+    if ret:
+        out.write(frame)
+        cv2.imshow('Video', frame)
+    car.Car_Run(50,50)
     if (cv2.waitKey(1) == ord('s')):
         car.Car_Stop()
         break
 
 capture.release()
+out.release()
 cv2.destroyAllWindows()
