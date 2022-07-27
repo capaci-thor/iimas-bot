11b9905ceb1d854ac75c4ce8e4a7f55f39934f28
rot-jeot <rotjeot@gmail.com>
2022-07-26 22:56:01 -0500

    filtro adaptaive

--- opencvFiltro.py
+++ opencvFiltro.py
@@ -8,7 +8,7 @@ while (capture.isOpened()):
     frame = cv2.medianBlur(frame, 5)
     gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    ret,th1 = cv2.adaptiveThreshold(gray_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+    th1 = cv2.adaptiveThreshold(gray_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
 
 
     cv2.imshow('webCam',th1)
