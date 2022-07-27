a7058d30fffc648589ab5f608c04d7341b9605ba
rot-jeot <rotjeot@gmail.com>
2022-07-26 22:54:51 -0500

    filtro adaptaive

--- opencvFiltro.py
+++ opencvFiltro.py
@@ -5,6 +5,7 @@ capture = cv2.VideoCapture(0)
 while (capture.isOpened()):
 
     ret, frame = capture.read()
+    frame = cv2.medianBlur(frame, 5)
     gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     ret,th1 = cv2.adaptiveThreshold(gray_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
