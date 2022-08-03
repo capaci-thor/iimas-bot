547d10c6743da768eae6505fec3f87ece4c4bae9
rot-jeot <rotjeot@gmail.com>
2022-08-02 22:58:04 -0500

    apagado

--- main.py
+++ main.py
@@ -4,17 +4,24 @@ import time
 
 car = YB_Pcb_Car.YB_Pcb_Car()
 
+a = 0
 
-
-while(True):
+while(a == 0):
 
     distance = dis.Distance_test()
+    a = input()
+    if(a != 0):
+        car.Car_Stop()
 
     if(distance > 50):
-        car.Car_Run(250,250)
+        car.Car_Run(100,100)
 
     elif( (distance < 50) and (distance > 5)):
         vel = car.Car_Run(int(distance),int(distance))
     elif(distance < 5):
         car.Car_Stop()
+
+    car.Car_Stop()
+
+
 
