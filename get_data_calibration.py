3b6d69177be334d39f90d5904aae838530870b7b
Eduardo Ortega <rotjeot@gmail.com>
2023-08-21 11:23:00 -0600

    Archivo para obtener dato de encoders

---
+++ get_data_calibration.py
@@ -0,0 +1,67 @@
+from serial import *
+import smbus
+from time import sleep
+import math
+
+ser = Serial(
+        port='/dev/ttyAMA0',
+        baudrate = 9600,
+        parity=PARITY_NONE,
+        stopbits=STOPBITS_ONE,
+        bytesize=EIGHTBITS,
+        timeout=10
+        )
+
+I2C_SLAVE_ADDRESS = 0x8 #Arduino was configured for this adress
+
+file = open('read.csv', 'w')
+
+def get_msg():
+    diametro = 6.6
+    global ser
+    vector = []
+
+
+    #code for right encoder
+    ser.write("0".encode())
+    c_r = ser.readline().decode()
+    c_r = c_r.replace("\n","")
+    c_r = c_r.replace("\r","")
+    vector.append(int(c_r))
+
+
+    #code for left encoder
+    bus = smbus.SMBus(1)
+    slaveAddress = I2C_SLAVE_ADDRESS
+
+    BytesToSend = ConvertStringsToBytes("1")
+    bus.write_byte(slaveAddress,  1)
+    try:
+        bus.write_byte(slaveAddress,  1)
+        c_l = bus.read_byte(slaveAddress)
+        c_l = int(c_l)            
+    except:
+        print("remote i/o error")
+        sleep(0.5)
+    vector.append(c_l)
+    rpm_r = (vector[0]/20)*60
+    rpm_l = (vector[1]/20)*60
+    vel_r = (math.pi*diametro*rpm_r)/(100*60)
+    vel_l = (math.pi*diametro*rpm_l)/(100*60)
+    return str(rpm_r)+ ','+ str(vel_r) + ','+ str(rpm_l)+ ','+ str(vel_l)+'\n'
+
+
+def ConvertStringsToBytes(src):
+    converted = []
+    for b in src:
+        converted.append(ord(b))
+    return converted   
+
+
+file.write('pwm,rpm_r,v_r,rpm_l,v_l\n')
+for i in range(0,255):
+    msg = get_msg()
+    to_save = str(i)+',' + msg
+    file.write(to_save)
+
+file.close()
