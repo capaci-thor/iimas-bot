70e32d694c74831f0ae738f944306ba468612b4f
Eduardo Ortega <rotjeot@gmail.com>
2024-01-22 09:28:35 -0600

    update umbral value

--- Lyapunov.py
+++ Lyapunov.py
@@ -226,8 +226,8 @@ def lyapunov():
         #    outL = int(outL * auxOutL)
         #    outR = int(outR * auxOutR)
 
-        maxVel = 40
-        maxUmbral = 40
+        maxVel = 50
+        maxUmbral = 50
         if (outL > maxUmbral): outL = maxVel
         if (outR > maxUmbral): outR = maxVel
         if (outL < -maxUmbral): outL = -maxVel
