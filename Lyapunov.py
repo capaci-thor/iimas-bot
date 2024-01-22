233fdfb8c70a56be5514566bcec3ee82bc371d47
Eduardo Ortega <rotjeot@gmail.com>
2024-01-21 23:49:39 -0600

    add max umbral limit 50

--- Lyapunov.py
+++ Lyapunov.py
@@ -226,8 +226,8 @@ def lyapunov():
             outL = int(outL * auxOutL)
             outR = int(outR * auxOutR)
 
-        maxVel = 70
-        maxUmbral = 70
+        maxVel = 50
+        maxUmbral = 50
         if (outL > maxUmbral): outL = maxVel
         if (outR > maxUmbral): outR = maxVel
         if (outL < -maxUmbral): outL = -maxVel
